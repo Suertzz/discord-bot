28c319224258571a3faa26f3310632eb8e4b2eac
Suertzz <suertzz@DESKTOP-RPHJKKS.localdomain>
2019-06-12 03:59:19 +0200

    issue omsg is not defined fix

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -2,7 +2,7 @@ const discord = require('discord.js');
 exports.run = (client, fs, config, messageDelete) => {
     if (messageDelete.author.bot || !(messageDelete.content.indexOf(config.prefix) !== 0) || messageDelete.content <= 0)
         return;
-    if (omsg.content.length > 1023 || nmsg.content.length > 1023)
+    if (messageDelete.content.length > 1023)
         return;
     const log = client.channels.get("461275693808877568");
     const embed = new discord.RichEmbed()
