d9242d0574a0ddf29004fc798a265c29455f3191
Suertzz <suertzz@DESKTOP-RPHJKKS.localdomain>
2019-06-14 05:37:41 +0200

    reduce delete time on command say

--- commands/say.js
+++ commands/say.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args, fs, config) =>
     if (message.member.roles.some(r=>["Leader", "Administrateur"].includes(r.name))) {
         const emoji = client.emojis.find(x => x.name === "prof");
         message.react(emoji);
-        message.delete(60000);
+        message.delete(50);
         const sayMessage = args.join(" ");
         message.channel.send(sayMessage);
     } else {
